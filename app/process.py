9f631d6df18c33cc40a9a5bd5364185c9f60fb07
hungngo04 <ngotienhung195@gmail.com>
2022-10-17 00:58:59 -0400

    Call OpenAI by API Key

--- app/process.py
+++ app/process.py
@@ -3,4 +3,12 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(model="text-davinci-002", prompt="Say this is a test", temperature=0, max_tokens=6)
+subject = "dogs"
+prompt = f"Generate branding snippet for {subject}"
+
+response = openai.Completion.create(
+    engine="davinci-instruct-beta-v3", 
+    prompt=prompt, 
+    max_tokens=32)
+
+print(response)
