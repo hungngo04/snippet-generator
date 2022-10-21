03b7474bb2e6bad4725150e01594a127c780275f
hungngo04 <ngotienhung195@gmail.com>
2022-10-21 16:27:59 -0400

    parse data from input

--- app/process.py
+++ app/process.py
@@ -1,14 +1,29 @@
 import os
 import openai
+import argparse
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-subject = "dogs"
-prompt = f"Generate branding snippet for {subject}"
+def main():
+    print("Running")
 
-response = openai.Completion.create(
-    engine="davinci-instruct-beta-v3", 
-    prompt=prompt, 
-    max_tokens=32)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=str, required=True)
+    args = parser.parse_args()
+    user_input = args.input
 
-print(response)
+    print(f"User input: {user_input}")
+    pass
+
+# subject = "dogs"
+# prompt = f"Generate branding snippet for {subject}"
+
+# response = openai.Completion.create(
+#     engine="davinci-instruct-beta-v3", 
+#     prompt=prompt, 
+#     max_tokens=32)
+
+# print(response)
+
+if __name__ == "__main__":
+    main()
