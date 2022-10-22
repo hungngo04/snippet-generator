5a2c5c14c1c54b24b5183c7fcc94309db6125077
hungngo04 <ngotienhung195@gmail.com>
2022-10-21 21:32:21 -0400

    return output in python

--- app/process.py
+++ app/process.py
@@ -1,29 +1,43 @@
+from http.client import ResponseNotReady
 import os
 import openai
 import argparse
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 def main():
-    print("Running")
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=str, required=True)
     args = parser.parse_args()
     user_input = args.input
 
     print(f"User input: {user_input}")
-    pass
+    result = generate_snippet(user_input)
+    print(result)
 
-# subject = "dogs"
-# prompt = f"Generate branding snippet for {subject}"
 
-# response = openai.Completion.create(
-#     engine="davinci-instruct-beta-v3", 
-#     prompt=prompt, 
-#     max_tokens=32)
 
-# print(response)
+def generate_snippet(prompt: str):
+    #Load API key from OpenAI
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    enriched_prompt = f"Generate upbeat branding snippet for {prompt}: "
+    response = openai.Completion.create(
+        engine="davinci-instruct-beta-v3", 
+        prompt=enriched_prompt, 
+        max_tokens=32
+    )
+
+    #Extract output
+    branding_text : str = response["choices"][0]["text"]
+
+    #Remove blank space
+    branding_text = branding_text.strip()
+
+    #Add "..." at the end of string
+    last_char = branding_text[-1]
+    if last_char not in {".", "!", "?"}:
+        branding_text += "..."
+
+    return branding_text
+
 
 if __name__ == "__main__":
     main()
