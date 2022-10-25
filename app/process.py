3213efdbded68b50877fb1ca07e0d08811ab2f09
hungngo04 <ngotienhung195@gmail.com>
2022-10-24 20:43:39 -0400

    generate keywords

--- app/process.py
+++ app/process.py
@@ -1,7 +1,9 @@
 from http.client import ResponseNotReady
+from typing import List
 import os
 import openai
 import argparse
+import re
 
 def main():
     parser = argparse.ArgumentParser()
@@ -11,8 +13,29 @@ def main():
 
     print(f"User input: {user_input}")
     result = generate_snippet(user_input)
+    keyword_result = generate_keyword(user_input)
+    print(keyword_result)
     print(result)
 
+def generate_keyword(prompt: str) -> List[str]:
+    #Load API key from OpenAI
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    enriched_prompt = f"Generate branding keywords for {prompt}: "
+    response = openai.Completion.create(
+        engine="davinci-instruct-beta-v3", 
+        prompt=enriched_prompt, 
+        max_tokens=32
+    )
+
+    #Extract output
+    keyword_text : str = response["choices"][0]["text"]
+
+    #Remove blank space
+    keyword_text = keyword_text.strip()
+
+    keyword_array = re.split(",|\n|;|-", keyword_text)
+    
+    return keyword_array
 
 
 def generate_snippet(prompt: str):
