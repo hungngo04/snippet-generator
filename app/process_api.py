47b84ea7d725c68783ffd43c7012152dda38fa54
hungngo04 <ngotienhung195@gmail.com>
2022-10-25 00:16:18 -0400

    GET method

--- app/process_api.py
+++ app/process_api.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from process import generate_snippet, generate_keyword
 
 app = FastAPI()
 
+@app.get("/generate_snippet")
+async def generate_snippet_api(prompt: str):
+    snippet = generate_snippet(prompt)
+    return {"snippet": snippet}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Hung"}
+@app.get("/generate_keyword")
+async def generate_keyword_api(prompt: str):
+    keyword = generate_keyword(prompt)
+    return {"keyword": keyword}
+
 
 #uvicorn process_api:app --reload
